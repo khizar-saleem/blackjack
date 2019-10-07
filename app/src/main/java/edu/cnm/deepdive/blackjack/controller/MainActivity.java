977274fbf157d5d4f6bb1957f9f7804b10fd1b20
Khizar Saleem <saleemkhizar@outlook.com>
2019-10-07 16:55:09 -0600

    Added fragments for different hands

--- app/src/main/java/edu/cnm/deepdive/blackjack/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/blackjack/controller/MainActivity.java
@@ -6,9 +6,12 @@ import android.widget.Button;
 import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.ViewModelProviders;
 import edu.cnm.deepdive.blackjack.R;
 import edu.cnm.deepdive.blackjack.model.entity.Card;
+import edu.cnm.deepdive.blackjack.model.entity.Hand;
 import edu.cnm.deepdive.blackjack.model.pojo.RoundWithDetails;
 import edu.cnm.deepdive.blackjack.viewmodel.MainViewModel;
 
@@ -18,16 +21,22 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    ListView cards = findViewById(R.id.cards);
-    ArrayAdapter<Card> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
-    cards.setAdapter(adapter);
     MainViewModel viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
     viewModel.getRound().observe(this, (round) -> {
-      adapter.clear();
-      adapter.addAll(round.getHands().get(0).getCards());
     });
     Button addDeck = findViewById(R.id.start_round);
     addDeck.setOnClickListener((view) -> viewModel.startRound());
+    setupFragments();
+  }
+
+  private void setupFragments() {
+    Fragment dealerFragment = new DealerHandFragment();
+    Fragment playerFragment = new PlayerHandFragment();
+    FragmentManager manager = getSupportFragmentManager();
+    manager.beginTransaction()
+        .replace(R.id.dealer_hand, dealerFragment)
+        .replace(R.id.player_hand, playerFragment)
+        .commit();
   }
 
 
