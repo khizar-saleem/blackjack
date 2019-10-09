962c0e09b976b9c1cd0e39209887d39a0b9b3aa6
Khizar Saleem <saleemkhizar@outlook.com>
2019-10-09 10:02:02 -0600

    Implemented "hit me" button

--- app/src/main/java/edu/cnm/deepdive/blackjack/controller/HandFragment.java
+++ app/src/main/java/edu/cnm/deepdive/blackjack/controller/HandFragment.java
@@ -19,6 +19,7 @@ import edu.cnm.deepdive.blackjack.viewmodel.MainViewModel;
 public abstract class HandFragment extends Fragment {
 
   private ArrayAdapter<Card> adapter;
+  private MainViewModel viewModel;
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -34,7 +35,7 @@ public abstract class HandFragment extends Fragment {
   @Override
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
-    MainViewModel viewModel = ViewModelProviders.of(getActivity()).get(MainViewModel.class);
+    viewModel = ViewModelProviders.of(getActivity()).get(MainViewModel.class);
     handToObserve(viewModel).observe(this, (hand) -> {
       adapter.clear();
       adapter.addAll(hand.getCards());
@@ -45,4 +46,7 @@ public abstract class HandFragment extends Fragment {
 
   public abstract int getLayout();
 
+  protected MainViewModel getViewModel() {
+    return viewModel;
+  }
 }
--- app/src/main/java/edu/cnm/deepdive/blackjack/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/blackjack/controller/MainActivity.java
@@ -1,18 +1,10 @@
 package edu.cnm.deepdive.blackjack.controller;
 
-import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProviders;
 import edu.cnm.deepdive.blackjack.R;
-import edu.cnm.deepdive.blackjack.model.entity.Card;
-import edu.cnm.deepdive.blackjack.model.entity.Hand;
-import edu.cnm.deepdive.blackjack.model.pojo.RoundWithDetails;
 import edu.cnm.deepdive.blackjack.viewmodel.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,17 +18,6 @@ public class MainActivity extends AppCompatActivity {
     });
     Button addDeck = findViewById(R.id.start_round);
     addDeck.setOnClickListener((view) -> viewModel.startRound());
-    setupFragments();
-  }
-
-  private void setupFragments() {
-    Fragment dealerFragment = new DealerHandFragment();
-    Fragment playerFragment = new PlayerHandFragment();
-    FragmentManager manager = getSupportFragmentManager();
-    manager.beginTransaction()
-        .replace(R.id.dealer_hand, dealerFragment)
-        .replace(R.id.player_hand, playerFragment)
-        .commit();
   }
 
 
